c5460b632f2d1743c60595b8d11c280b680e7243
TomoyaOnishi <tomoyaonishi@icloud.com>
2018-03-19 19:54:36 +0900

    material components for the webを導入してビルドが通った

--- source/js/App.js
+++ source/js/App.js
@@ -1,8 +1,10 @@
 import Sub from './Sub';
 import '../css/style.scss';
+import {MDCRipple} from '@material/ripple';
 
 window.test = function() {
     console.log('###window.test()');
     var sub = new Sub();
     sub.call();
+    const ripple = new MDCRipple(document.querySelector('.foo-button'));
 };
