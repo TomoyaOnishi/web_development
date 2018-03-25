c24a1455f26e0e940628261241b74bfab4a81e22
TomoyaOnishi <tomoyaonishi@icloud.com>
2018-03-25 14:57:17 +0900

    autoInit化した

--- source/js/App.js
+++ source/js/App.js
@@ -3,16 +3,32 @@ import '../css/style.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import * as mdc from 'material-components-web';
+
 class TestComponent extends React.Component {
 
     componentDidMount() {
-        // mdc.ripple.MDCRipple.attachTo(document.querySelector('.mdc-button'));
+        mdc.autoInit();
     }
 
     render() {
         return (
             <div>
-                <button className='mdc-button'>button</button>
+                <div className="mdc-checkbox">
+                    <input type="checkbox"
+                           className="mdc-checkbox__native-control"/>
+                    <div className="mdc-checkbox__background">
+                        <svg className="mdc-checkbox__checkmark"
+                             viewBox="0 0 24 24">
+                            <path className="mdc-checkbox__checkmark-path"
+                                  fill="none"
+                                  stroke="white"
+                                  d="M1.73,12.91 8.1,19.28 22.79,4.59"/>
+                        </svg>
+                        <div className="mdc-checkbox__mixedmark"></div>
+                    </div>
+                </div>
+                <button className='mdc-button mdc-button--raised' data-mdc-auto-init="MDCRipple">button</button>
                 <div className="mdc-text-field mdc-text-field--upgraded">
                     <input type="text" id="pre-filled" className="mdc-text-field__input"/>
                     <label className="mdc-floating-label mdc-floating-label--float-above" htmlFor="pre-filled">
@@ -27,6 +43,7 @@ class TestComponent extends React.Component {
 }
 
 ReactDOM.render(
-    <TestComponent/>,
+    <TestComponent/>
+    ,
     document.querySelector('#react-root')
 );
--- webpack.config.js
+++ webpack.config.js
@@ -54,14 +54,6 @@ module.exports = {
                     {
                         loader: 'sass-loader',
                         options: {
-                            importer: function (url) {
-                                if (url.indexOf('@material') === 0) {
-                                    var filePath = url.split('@material')[1];
-                                    var nodeModulePath = `./node_modules/@material/${filePath}`;
-                                    return {file: require('path').resolve(nodeModulePath)};
-                                }
-                                return {file: url};
-                            },
                             sourceMap: true,
                             includePaths: ['./node_modules/']
                         }
