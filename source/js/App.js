306cbca150ccb92b70fd404e457d6e515d3adedd
TomoyaOnishi <tomoyaonishi@icloud.com>
2018-03-25 15:45:48 +0900

    CSSは別のwebpackコンフィグにした

--- source/js/App.js
+++ source/js/App.js
@@ -1,4 +1,3 @@
-import '../css/style.scss';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,13 @@ const path = require('path');
 const paths = {
     PUBLIC: path.resolve(__dirname, 'public'),
     JS: path.resolve(__dirname, 'source/js'),
+    CSS: path.resolve(__dirname, 'source/css')
 };
 
 console.log('related paths: \n', paths);
 
-module.exports = {
+module.exports = [{
+    devtool: 'source-map',
     entry: path.join(paths.JS + '/App.js'),
     output: {
         path: paths.PUBLIC,
@@ -36,31 +38,40 @@ module.exports = {
                 ]
             },
 
-            {
-                test: /\.scss/,
-                use: [
-
-                    'style-loader',
-
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            url: false,
-                            importLoaders: 2
-                        },
-
-                    },
-
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            sourceMap: true,
-                            includePaths: ['./node_modules/']
-                        }
-                    }
-                ]
-            }
-
         ]
     }
-};
+}];
+
+
+module.exports.push({
+    entry: paths.CSS + '/style.scss',
+    output: {
+        // This is necessary for webpack to compile
+        // But we never use style-bundle.js
+        path: paths.PUBLIC,
+        filename: 'ignore-style-bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: [
+                {
+                    loader: 'file-loader',
+                    options: {
+                        name: 'bundle.css',
+                    },
+                },
+                {loader: 'extract-loader'},
+                {loader: 'css-loader'},
+                {
+                    loader: 'sass-loader',
+                    options: {
+                        sourceMap: true,
+                        includePaths: ['./node_modules/']
+                    }
+
+                },
+            ]
+        }]
+    },
+});
